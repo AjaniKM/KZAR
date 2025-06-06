2339525166aff6735b110f0f9b953b22160418af
AjaniKM <ajanikm1@gmail.com>
2025-06-06 11:19:28 -0400

    Add files via upload

---
+++ www.kzar.com/findPorosity.js
@@ -0,0 +1,3 @@
+const imageContainer = document.getElementById('imageContainer');
+const image = document.createElement('img');
+image.src = "low_porosity_480x480.webp"
---
+++ www.kzar.com/index.js
@@ -0,0 +1,8 @@
+const img = document.createElement('img');
+img.src = "KZARR logo.jpeg";
+img.alt = 'KZAR Logo';
+img.style.width = '35%';
+img.style.height = '35%';
+img.align = 'center';
+document.getElementById('logoContainer').appendChild(img);
+
