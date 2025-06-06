7df516755cf5cf58640aea4e771132b02b3719e4
AjaniKM <ajanikm1@gmail.com>
2025-06-06 11:31:45 -0400

    Delete www.kzar.com directory

--- www.kzar.com/findPorosity.js
+++
@@ -1,3 +0,0 @@
-const imageContainer = document.getElementById('imageContainer');
-const image = document.createElement('img');
-image.src = "low_porosity_480x480.webp"
--- www.kzar.com/index.js
+++
@@ -1,8 +0,0 @@
-const img = document.createElement('img');
-img.src = "KZARR logo.jpeg";
-img.alt = 'KZAR Logo';
-img.style.width = '35%';
-img.style.height = '35%';
-img.align = 'center';
-document.getElementById('logoContainer').appendChild(img);
-
